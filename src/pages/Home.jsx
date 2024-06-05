b5f2493445cc6f40b2fcd5bcfc01faddb72d7326
blackReapr <mahammadea@code.edu.az>
2024-06-05 16:47:50 +0400

    feat: Welcome added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,6 +1,8 @@
+import { Link } from "react-router-dom";
 import ArtGallery from "../components/Home/ArtGallery";
 import Carousel from "../components/Home/Carousel";
 import ProductsInfo from "../components/Home/ProductsInfo";
+import Welcome from "../assets/images/welcome.jpg";
 
 const Home = () => {
   return (
@@ -19,6 +21,27 @@ const Home = () => {
       </section>
       <section className="artGallery">
         <ArtGallery />
+      </section>
+      <section className="welcome">
+        <div className="container">
+          <div className="row">
+            <div className="leftSide">
+              <h3 className="title">WELCOME TO PIQUANT</h3>
+              <p className="info">
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+                eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+                enim ad minim veniam, quis nostrud exercitation ullamco laboris
+                nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor
+                in reprehenderit in voluptate velit esse cillum dolore eu fugiat
+                nulla pariatur.
+              </p>
+              <Link to="/"> Read More </Link>
+            </div>
+            <div className="rightSide">
+              <img src={Welcome} alt="" />
+            </div>
+          </div>
+        </div>
       </section>
     </>
   );
