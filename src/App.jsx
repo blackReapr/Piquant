6bfa3974495342d6aa64fe4c63bc0c74c633bbf5
blackReapr <mahammadea@code.edu.az>
2024-06-06 17:20:48 +0400

    Hero Section Fix

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { Route, Routes } from "react-router-dom";
+import { Link, Route, Routes } from "react-router-dom";
 
 import "./assets/scss/index.scss";
 
@@ -8,8 +8,23 @@ import { Home } from "./pages";
 // Components
 import Header from "./components/Header/Header";
 import { Toaster } from "react-hot-toast";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [isActive, setIsActive] = useState(false);
+
+  useEffect(() => {
+    const onScroll = () => {
+      if (window.scrollY > 400) setIsActive(true);
+      else {
+        setIsActive(false);
+      }
+    };
+    onScroll();
+    window.addEventListener("scroll", onScroll);
+    return () => window.removeEventListener("scroll", onScroll);
+  }, []);
+
   return (
     <>
       <Header />
@@ -19,6 +34,9 @@ function App() {
         </Routes>
       </main>
       <Toaster position="top-center" reverseOrder={false} />
+      <a href="#" className={`toTop ${isActive ? "active" : ""}`}>
+        TOP
+      </a>
     </>
   );
 }
