6924422ec97d8db2511125f0be895c258f2454cc
blackReapr <mahammadea@code.edu.az>
2024-06-06 17:02:10 +0400

    Header color fixed

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,11 @@
+const Footer = () => {
+  return (
+    <footer className="footer">
+      <div className="container">
+        <div className="row"></div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
